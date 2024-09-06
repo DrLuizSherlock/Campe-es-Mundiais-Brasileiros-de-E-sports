03eee2d98a0580b5ec091bf0ec42a638b616bb52
DrLuizSherlock <1luiz.gustavo.tozzi1@gmail.com>
2024-09-06 15:38:12 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+        titulo: "brTT",
+        descricao: "Considerado por muitos o maior jogador brasileiro de LoL de todos os tempos, brTT possui uma vasta experiência e diversas conquistas em sua carreira.",
+        link: "https://pt.wikipedia.org/wiki/BrTT",
+        tags: "tt brtt brt lol leagueoflegends"
+    },
+    {
+        titulo: "dyNquedo",
+        descricao: "Um dos jovens talentos mais promissores do Brasil, dyNquedo já conquistou diversos títulos importantes e se destaca pelo seu estilo de jogo agressivo.",
+        link: "https://lol.fandom.com/wiki/DyNquedo",
+        tags: "dynkas dynquedo lol leagueoflegends"
+    },
+    {
+        titulo: "Fallen",
+        descricao: "Um dos maiores nomes do CS:GO mundial, Fallen já conquistou diversos títulos importantes, incluindo Majors, e é considerado um dos melhores jogadores da história do jogo.",
+        link: "https://pt.wikipedia.org/wiki/FalleN",
+        tags: "fallen verdadeiro cs counterstrike sniper velho"
+    },
+    {
+        titulo: "Nobru",
+        descricao: "Um dos maiores nomes do Free Fire mundial, Nobru conquistou diversos títulos importantes e é considerado uma lenda do jogo.",
+        link: "https://pt.wikipedia.org/wiki/Nobru",
+        tags: "nobru freefire mobile game"
+    },
+    {
+        titulo: "coldzera",
+        descricao: "Outro brasileiro que fez história no CS:GO, coldzera se destacou por suas jogadas incríveis e sua capacidade de liderar equipes.",
+        link: "https://pt.wikipedia.org/wiki/Coldzera",
+        tags: "cold colzera coldzera coldzuera cs counterstrike sniper velho"
+    },
+    {
+        titulo: "Aspas",
+        descricao: "Um dos principais jogadores brasileiros de VALORANT, Aspas foi fundamental para a conquista do primeiro título mundial de uma equipe brasileira na modalidade.",
+        link: "https://liquipedia.net/valorant/Aspas",
+        tags: "aspas aspa valorant vava pro gira"
+    }
+];
